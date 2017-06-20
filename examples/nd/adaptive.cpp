51ac92ea6e091f9778ce6ead560acb502d95bd73
Tom Peterka <tpeterka@tpeterka-macbook-pro.mcs.anl.gov>
2017-06-19 19:14:23 -0500

    testing

--- examples/nd/adaptive.cpp
+++ examples/nd/adaptive.cpp
@@ -103,39 +103,13 @@ int main(int argc, char** argv)
 //                    { b->generate_sinc_data(cp, d_args); });
 
     // 2d sinc function f(x,y) = sinc(x)sinc(y)
-//     float norm_err_limit = 1.0e-2;           // normalized maximum allowable error
-//     d_args.pt_dim       = 3;
-//     d_args.dom_dim      = 2;
-//     d_args.p[0]         = 4;
-//     d_args.p[1]         = 4;
-//     d_args.ndom_pts[0]  = 100;
-//     d_args.ndom_pts[1]  = 100;
-//     d_args.nctrl_pts[0] = 20;
-//     d_args.nctrl_pts[1] = 20;
-//     d_args.min[0]       = -4.0 * M_PI;
-//     d_args.min[1]       = -4.0 * M_PI;
-//     d_args.max[0]       = 4.0 * M_PI;
-//     d_args.max[1]       = 4.0 * M_PI;
-//     d_args.s            = 10.0;              // scaling factor on range
-//     master.foreach([&](Block* b, const diy::Master::ProxyWithLink& cp)
-//                    { b->generate_sinc_data(cp, d_args); });
-
-    // --- experiments for the paper ---
-
-    // 2d sinc function
-//     float norm_err_limit = 1.0e0;
-//     float norm_err_limit = 1.0e-1;
-//     float norm_err_limit = 1.0e-2;
-//     float norm_err_limit = 1.0e-3;
-    float norm_err_limit = 1.0e-4;
-//     float norm_err_limit = 1.0e-5;
-//     float norm_err_limit = 1.0e-6;
+    float norm_err_limit = 1.0e-5;           // normalized maximum allowable error
     d_args.pt_dim       = 3;
     d_args.dom_dim      = 2;
     d_args.p[0]         = 4;
     d_args.p[1]         = 4;
-    d_args.ndom_pts[0]  = 100;
-    d_args.ndom_pts[1]  = 100;
+    d_args.ndom_pts[0]  = 500;
+    d_args.ndom_pts[1]  = 500;
     d_args.nctrl_pts[0] = 5;
     d_args.nctrl_pts[1] = 5;
     d_args.min[0]       = -4.0 * M_PI;
@@ -145,6 +119,32 @@ int main(int argc, char** argv)
     d_args.s            = 10.0;              // scaling factor on range
     master.foreach([&](Block* b, const diy::Master::ProxyWithLink& cp)
                    { b->generate_sinc_data(cp, d_args); });
+
+    // --- experiments for the paper ---
+
+    // 2d sinc function
+//     float norm_err_limit = 1.0e0;
+//     float norm_err_limit = 1.0e-1;
+//     float norm_err_limit = 1.0e-2;
+//     float norm_err_limit = 1.0e-3;
+//     float norm_err_limit = 1.0e-4;
+//     float norm_err_limit = 1.0e-5;
+//     float norm_err_limit = 1.0e-6;
+//     d_args.pt_dim       = 3;
+//     d_args.dom_dim      = 2;
+//     d_args.p[0]         = 4;
+//     d_args.p[1]         = 4;
+//     d_args.ndom_pts[0]  = 100;
+//     d_args.ndom_pts[1]  = 100;
+//     d_args.nctrl_pts[0] = 5;
+//     d_args.nctrl_pts[1] = 5;
+//     d_args.min[0]       = -4.0 * M_PI;
+//     d_args.min[1]       = -4.0 * M_PI;
+//     d_args.max[0]       = 4.0 * M_PI;
+//     d_args.max[1]       = 4.0 * M_PI;
+//     d_args.s            = 10.0;              // scaling factor on range
+//     master.foreach([&](Block* b, const diy::Master::ProxyWithLink& cp)
+//                    { b->generate_sinc_data(cp, d_args); });
 
    // 3d sinc function
 //     float norm_err_limit = 1.0e-1;
@@ -191,20 +191,20 @@ int main(int argc, char** argv)
     // 3d S3D
 //     float norm_err_limit = 1.0e0;
 //     float norm_err_limit = 1.0e-1;
-    float norm_err_limit = 1.0e-2;
-    d_args.pt_dim       = 4;
-    d_args.dom_dim      = 3;
-    d_args.p[0]         = 3;
-    d_args.p[1]         = 3;
-    d_args.p[2]         = 3;
-    d_args.ndom_pts[0]  = 704;
-    d_args.ndom_pts[1]  = 540;
-    d_args.ndom_pts[2]  = 550;
-    d_args.nctrl_pts[0] = 140;
-    d_args.nctrl_pts[1] = 108;
-    d_args.nctrl_pts[2] = 110;
-    master.foreach([&](Block* b, const diy::Master::ProxyWithLink& cp)
-                   { b->read_3d_file_data(cp, d_args); });
+//     float norm_err_limit = 1.0e-2;
+//     d_args.pt_dim       = 4;
+//     d_args.dom_dim      = 3;
+//     d_args.p[0]         = 3;
+//     d_args.p[1]         = 3;
+//     d_args.p[2]         = 3;
+//     d_args.ndom_pts[0]  = 704;
+//     d_args.ndom_pts[1]  = 540;
+//     d_args.ndom_pts[2]  = 550;
+//     d_args.nctrl_pts[0] = 140;
+//     d_args.nctrl_pts[1] = 108;
+//     d_args.nctrl_pts[2] = 110;
+//     master.foreach([&](Block* b, const diy::Master::ProxyWithLink& cp)
+//                    { b->read_3d_file_data(cp, d_args); });
 
     double encode_time = MPI_Wtime();
 
