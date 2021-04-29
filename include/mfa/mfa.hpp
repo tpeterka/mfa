d8cc8661edbfe93e7fe5eb4b415ec3f9fc29515e
Tom Peterka <tpeterka@mcs.anl.gov>
2021-04-29 10:49:15 -0500

    Rebased to tom-tspline

--- include/mfa/mfa.hpp
+++ include/mfa/mfa.hpp
@@ -27,7 +27,7 @@
 // #define WEIGH_ALL_DIMS
 
 // comment out the following line for original single tensor product version
-#define MFA_TMESH
+// #define MFA_TMESH
 
 // linear least squares local solve
 #define MFA_LINEAR_LOCAL
