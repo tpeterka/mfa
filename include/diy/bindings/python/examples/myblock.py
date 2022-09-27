c204110234fbc592edc957eabb7cdca6eee8e779
Tom Peterka <tpeterka@mcs.anl.gov>
2022-09-27 11:12:05 -0500

    Cleaned out external dependencies
    Prior to merge w/ peru branch

--- include/diy/bindings/python/examples/myblock.py
+++
@@ -1,9 +0,0 @@
-import diy
-
-w = diy.mpi.MPIComm()
-m = diy.Master(w)
-
-diy.add_my_block(m, 0)
-diy.add_my_block(m, 5)
-
-print(m)
--- include/diy/conf/ci/travis-ci/build-docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-doxygen
-pip3 install doctr
-doctr deploy --built-docs docs/html --gh-pages-docs .
-
