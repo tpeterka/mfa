2419527b93b015dbd307f54b211db74214a73086
Tom Peterka <tpeterka@mcs.anl.gov>
2022-10-21 09:46:30 -0500

    checking tom-tspline w/o MFA_TMESH defined
    - fixed calls to FindSpan in Decoder
    - all tests pass

--- include/mfa/decode.hpp
+++ include/mfa/decode.hpp
@@ -1455,7 +1455,7 @@ exit(1);
             // This small loop accounts for ~40% of the total time for this method (measured 11/16/21 for 3d, p=4, ctrlpts=30)
             for (int i = 0; i < dom_dim; i++)
             {
-                di.span[i] = mfa_data.FindSpan(i, param(i));
+                di.span[i] = mfa_data.tmesh.FindSpan(i, param(i), tensor);
                 mfa_data.FastBasisFunsDers(i, param(i), di.span[i], 1, di.D[i], di.bfi);
             }
 
@@ -1584,7 +1584,7 @@ exit(1);
             // compute spans and basis functions for the given parameters
             for (int i = 0; i < dom_dim; i++)
             {
-                di.span[i] = mfa_data.FindSpan(i, param(i));
+                di.span[i] = mfa_data.tmesh.FindSpan(i, param(i), tensor);
 
                 mfa_data.FastBasisFuns(i, param(i), di.span[i], di.N[i], di.bfi);
             }
