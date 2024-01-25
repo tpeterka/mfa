08996fdd62a0a4f434fc8c8e697461ecb9c5dfb6
David Lenz <david.c.lenz.1@gmail.com>
2024-01-25 14:41:48 -0600

    Add build options and expand spack recipe

--- packages/mfa/package.py
+++ packages/mfa/package.py
@@ -14,17 +14,28 @@ class Mfa(CMakePackage):
 
     version('master', branch='master')
 
+    variant('build_type', default='Release', description='CMake build type', values=('Debug', 'Release', 'RelWithDebInfo', 'MinSizeRel'))
     variant('thread', values=str, default='serial', description='Threading model: serial, tbb, sycl, kokkos (default = serial).')
+    variant('examples', default=True, description='Build MFA examples')
+    variant('tests', default=True, description='Build MFA unit tests')
 
-    depends_on('mpi')
+    depends_on('mpich')
     depends_on('tbb', when='thread=tbb')
     depends_on('kokkos', when='thread=kokkos')
 
     def cmake_args(self):
-        args = []
+        args = ['-DCMAKE_BUILD_TYPE=%s' % self.spec.variants['build_type'].value,
+                '-DCMAKE_C_COMPILER=%s' % self.spec['mpich'].mpicc,
+                '-DCMAKE_CXX_COMPILER=%s' % self.spec['mpich'].mpicxx,
+                self.define_from_variant('examples', 'mfa_build_examples'),
+                self.define_from_variant('tests', 'mfa_build_tests'),
+                '-Dmfa_thread=%s' % self.spec.variants['thread']]
 
-        thread = str(self.spec.variants['thread'].value)
-        print('Building with thread =', thread)
-        args.extend(['-Dmfa_thread=%s' % thread])
+        # thread = str(self.spec.variants['thread'].value)
+        print('MFA build type: ', self.spec.variants['build_type'].value)
+        print('Building MFA examples: ', self.spec.variants['examples'].value)
+        print('Building MFA tests: ', self.spec.variants['tests'].value)
+        print('Building MFA with threading type: ', self.spec.variants['thread'].value)
+        # args.extend(['-Dmfa_thread=%s' % thread])
 
         return args
