caf91de5ac201696653072857c4362829ca4dfd8
David Lenz <david.c.lenz.1@gmail.com>
2026-02-27 10:02:48 -0600

    Fixes python example to use new function signature

--- python/examples/evaluate.py
+++ python/examples/evaluate.py
@@ -6,17 +6,18 @@ import mfa
 import numpy as np
 
 # MPI, DIY world and master
-w = diy.mpi.MPIComm()           # world
-m = diy.Master(w)               # master
+w = diy.mpi.MPIComm()  # world
+m = diy.Master(w)  # master
 
 # load the results and print them out
 print("\n\nLoading blocks and printing them out\n")
 a = diy.ContiguousAssigner(w.size, -1)
-diy.read_blocks("approx.mfa", a, m, load = mfa.load_block)
-m.foreach(lambda b,cp: b.print_block(cp, False))
+diy.read_blocks("approx.mfa", a, m, load=mfa.load_block)
+m.foreach(lambda b, cp: b.print_block(cp, False))
 
 # evaluate a point
-param   = np.array([0.5, 0.5])          # input parameters where to decode the point
-pt      = np.array([0.0, 0.0, 0.0])     # assigning fake values defines shape and type
-m.foreach(lambda b, cp: b.decode_point(cp, param, pt))
+param = np.array([0.5, 0.5])  # input parameters where to decode the point
+pts_by_gid = {}
+m.foreach(lambda b, cp: pts_by_gid.update({cp.gid(): b.decode_point(cp, param)}))
+pt = pts_by_gid[0] if 0 in pts_by_gid else pts_by_gid[min(pts_by_gid)]
 print("\nThe point at [u =", param[0], ", v =", param[1], "] =", pt)
